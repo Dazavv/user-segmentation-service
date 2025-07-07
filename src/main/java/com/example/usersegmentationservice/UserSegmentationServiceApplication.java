617f5343964de7e25b4d0ee3750a8f1258589065
dazavv <dasha7348@gmail.com>
2025-07-07 13:34:41 +0300

    Initial commit

---
+++ src/main/java/com/example/usersegmentationservice/UserSegmentationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersegmentationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSegmentationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSegmentationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usersegmentationservice/UserSegmentationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersegmentationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSegmentationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
