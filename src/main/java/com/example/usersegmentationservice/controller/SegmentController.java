f3cb3aac6443613055e4c37a4e8c92c91ba40f64
dazavv <dasha7348@gmail.com>
2025-08-10 15:38:41 +0300

    new methods added

--- src/main/java/com/example/usersegmentationservice/controller/SegmentController.java
+++ src/main/java/com/example/usersegmentationservice/controller/SegmentController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -20,8 +21,8 @@ public class SegmentController {
     }
 
     @GetMapping
-    public Page<Segment> getAllSegments(@PageableDefault(sort = "code") Pageable pageable) {
-        return segmentService.getAllSegments(pageable);
+    public List<Segment> getAllSegments() {
+        return segmentService.getAllSegments();
     }
 
     @GetMapping(path = "{code}")
@@ -34,17 +35,24 @@ public class SegmentController {
         return segmentService.addNewSegment(segment);
     }
 
-    @PostMapping("/segments/{code}/distribute")
-    public void distributeSegmentToUsers(@RequestBody String code,
-                                            @RequestParam int percent) {
+    @PostMapping("{code}")
+    public void addUsersToSegment(@PathVariable String code,
+                                  @RequestParam Set<User> users) {
+        segmentService.addUsersToSegment(code, users);
+    }
+
+    @PostMapping("{code}/distribute")
+    public void distributeSegmentToUsers(@PathVariable String code,
+                                         @RequestParam int percent) {
         segmentService.assignSegmentToRandomUsers(code, percent);
     }
 
     @PutMapping(path = "{code}")
-    public Segment changeSegment(@RequestParam String code,
+    public void changeSegment(@PathVariable String code,
+                                 @RequestParam(required = false) String newCode,
                                  @RequestParam(required = false) String info,
                                  @RequestParam(required = false) Set<User> users) {
-        return segmentService.changeSegment(code, info, users);
+        segmentService.changeSegment(code, newCode, info, users);
     }
 
     @DeleteMapping(path = "{code}")
@@ -52,4 +60,9 @@ public class SegmentController {
         segmentService.deleteSegment(code);
     }
 
+    @DeleteMapping(path = "{code}")
+    public void deleteUsersFromSegment(@PathVariable String code,
+                                       @RequestParam Set<User> users) {
+        segmentService.deleteUsersFromSegment(code, users);
+    }
 }
